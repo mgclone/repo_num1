6e08e2caff42609bf7eb4163938d28690eea6581
fausto <fausto@fausto>
2017-04-23 19:32:58 -0400

    foward to VS

--- repo1_proj/repo1_proj/repo1_src.cpp
+++ repo1_proj/repo1_proj/repo1_src.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
 
-	cout << "repositories have to be cloned to visual studio" << endl;
+	cout << "repositories have to be cloned to visual studio. Ok this was succesful, foward to VS." << endl;
 	return 0;
 }
