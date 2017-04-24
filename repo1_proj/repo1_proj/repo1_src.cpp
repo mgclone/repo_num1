468fa47fae93a7f81622febfcfcb763eea10686f
mgclone <From@ VM.com>
2017-04-23 21:54:16 -0400

    changed branch 1

--- repo1_proj/repo1_proj/repo1_src.cpp
+++ repo1_proj/repo1_proj/repo1_src.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
 
 	cout << "repositories have to be cloned to visual studio. Ok this was succesful, foward to VS. branch local!" << endl;
-	cout << "this is branch 1, which, accidentally points to branch2branch" << endl;
+	cout << "this is branch 11, check, which, accidentally points to branch2branch" << endl;
 	return 0;
 }
