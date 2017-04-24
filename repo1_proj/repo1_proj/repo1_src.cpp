d486d5fad5671e646bb8d6e5624dfe8f62537ce6
mgclone <From@ VM.com>
2017-04-23 21:06:52 -0400

    made locally

--- repo1_proj/repo1_proj/repo1_src.cpp
+++ repo1_proj/repo1_proj/repo1_src.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(){
 
-	cout << "repositories have to be cloned to visual studio. Ok this was succesful, foward to VS." << endl;
+	cout << "repositories have to be cloned to visual studio. Ok this was succesful, foward to VS. branch local!" << endl;
 	return 0;
 }
