2e373dda23c25e3d77877981539165dedc43a778
mgclone <From@ VM.com>
2017-04-23 21:28:49 -0400

    changes to parent branch 2

--- repo1_proj/repo1_proj/repo1_src.cpp
+++ repo1_proj/repo1_proj/repo1_src.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
 
 	cout << "repositories have to be cloned to visual studio. Ok this was succesful, foward to VS. branch local!" << endl;
+	cout << "this is branch 2 parent" << endl;
 	return 0;
 }
